bfbc77c961bd3236c40419bdb8aec2d4122f4ff8
junjie <568692092@qq.com>
2022-02-23 18:19:30 +0800

    Update BAIMIAOZHENGJIANZHAOINFO.js

--- JavaSrcipt/BAIMIAOZHENGJIANZHAOINFO.js
+++ JavaSrcipt/BAIMIAOZHENGJIANZHAOINFO.js
@@ -1,7 +1,7 @@
 let obj = JSON.parse($response.body);
 obj.value.vip = true;
 obj.value.defaultRecognize.defaultNormal = -100;
-obj.value.recognize.defaultBatch = -100;
-obj.value.recognize.defaultTranslate = -100;
-obj.value.recognize.recognizeTranslateAll = 1;
+obj.value.defaultRecognize.defaultBatch = -100;
+obj.value.defaultRecognize.defaultTranslate = -100;
+obj.value.defaultRecognize.recognizeTranslateAll = 1;
 $done({ body: JSON.stringify(obj) });
