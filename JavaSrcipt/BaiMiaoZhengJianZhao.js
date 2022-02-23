ee3416eb6b27b7d3ddb82fb83b38fd5e756efcde
junjie <568692092@qq.com>
2022-02-23 20:39:00 +0800

    Update BaiMiaoZhengJianZhao.js

--- JavaSrcipt/BaiMiaoZhengJianZhao.js
+++ JavaSrcipt/BaiMiaoZhengJianZhao.js
@@ -1,27 +1,15 @@
 let obj = JSON.parse($response.body);
-obj.value.vip = {
-    "boughtType": "new",
-    "boughtUnit": "year",
-    "levelId": 2,
-    "boughtTime": 1542095038,
-    "boughtDuration": 10,
-    "orderId": 97262,
-    "operatorId": 0,
-    "level": {
-        "id": 2,
-        "description": "",
-        "recognizeTranslateAll": 1,
-        "recognizeBatch": -100,
-        "seq": 2,
-        "recognizeTranslate": -100,
-        "name": "黄金会员",
-        "recognizeNormal": -100
-    },
-    "deadlineNotified": 0,
-    "deadline": 1857714238,
-    "boughtAmount": 25
-}
-obj.value.defaultRecognize.defaultBatch = -100;
+obj.value.vip = true;
+obj.level = JSON.stringify({
+    "id": 2,
+    "recognizeTranslateAll": 1,
+    "recognizeBatch": -100,
+    "seq": 2,
+    "recognizeTranslate": -100,
+    "name": "黄金会员",
+    "recognizeNormal": -100
+}),
+    obj.value.defaultRecognize.defaultBatch = -100;
 obj.value.defaultRecognize.defaultTranslate = -100;
 obj.value.defaultRecognize.recognizeTranslateAll = 1;
 $done({ body: JSON.stringify(obj) });
